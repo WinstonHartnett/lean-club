04818a0cce25be5e473c53806354765bfaf2391d
Jack Nugent <jacknugent1529@gmail.com>
2022-05-09 20:03:00 -0500

    multiplication

--- Arithmetic.lean
+++ Arithmetic.lean
@@ -4,6 +4,7 @@ open Nat_
 
 theorem add_zero (m : Nat_) : m + zero = m := rfl
 
+@[simp]
 theorem add_succ (m n : Nat_) : m + succ n = succ (m + n) := rfl
 
 theorem zero_add (m : Nat_) : zero + m = m := by
@@ -13,7 +14,8 @@ theorem zero_add (m : Nat_) : zero + m = m := by
     rw [add_succ]
     rw [hyp]
 
-theorem succ_add_eq_add_succ (m n : Nat_) : succ m + n = m + succ n := by
+@[simp]
+theorem succ_add (m n : Nat_) : succ m + n = succ (m + n) := by
   induction n with
   | zero => 
     rfl
@@ -21,3 +23,60 @@ theorem succ_add_eq_add_succ (m n : Nat_) : succ m + n = m + succ n := by
     rw [add_succ]
     rw [hyp]
     rw [←add_succ]
+
+@[simp]
+theorem add_comm (m n : Nat_) : m + n = n + m := by
+  induction n with
+  | zero => 
+    rw [zero_add]
+    rfl
+  | succ n hyp =>
+    rw [succ_add]
+    rw [add_succ] 
+    rw [hyp]
+    
+@[simp]
+theorem add_assoc (l m n : Nat_) : (l + m) + n = l + (m + n) := by
+  induction n with
+  | zero => 
+    rw [add_zero]
+    rw [add_zero]
+  | succ n hyp => 
+    rw [add_succ]
+    rw [add_succ]
+    rw [add_succ]
+    rw [hyp]
+
+theorem mul_succ (m n : Nat_) : m * (succ n) = m + m * n := rfl
+
+theorem zero_mul (n : Nat_) : zero * n = zero := by
+  induction n with
+  | zero => rfl
+  | succ n hyp => 
+    rw [mul_succ]
+    rw [hyp]
+    rfl
+
+theorem succ_mul (m n : Nat_) : (succ m) * n = n + m * n := by
+  induction n with
+  | zero => rfl
+  | succ n hyp =>
+    rw [mul_succ]
+    rw [hyp]
+    rw [mul_succ]
+    rw [succ_add]
+    rw [succ_add]
+    rw [←add_assoc]
+    rw [←add_assoc]
+    simp
+    
+
+theorem mul_comm (m n : Nat_) : m * n = n * m := by
+  induction m with
+  | zero => 
+    rw [zero_mul]
+    rfl
+  | succ m hyp => 
+    rw [mul_succ]
+    rw [succ_mul]
+    rw [hyp]
--- Arithmetic/definition.lean
+++ Arithmetic/definition.lean
@@ -7,12 +7,22 @@ namespace Nat_
 
 def add (m n : Nat_) : Nat_ :=
   match n with
-  | Nat_.zero => m
-  | Nat_.succ n => Nat_.succ (add m n)
+  | zero => m
+  | succ n => succ (add m n)
 
 -- #eval add (succ (succ zero)) (succ zero)
-
 instance : Add Nat_ where
   add := add
+
+def multiply (m n : Nat_) : Nat_ :=
+  match n with
+  | zero => zero
+  | succ n => m + (multiply m n)
+
+instance : HMul Nat_ Nat_ Nat_ where
+  hMul := multiply
+
+#eval multiply (succ (succ zero)) (succ (succ zero))
+#eval (succ (succ zero)) * (succ (succ zero))
 
 end Nat_
