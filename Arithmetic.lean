54694f3c34ca6bf3e5f144c34af3c37e66e8ba63
Jack Nugent <jacknugent1529@gmail.com>
2022-05-11 11:04:23 -0500

    arithmetic.pie finished

--- Arithmetic.lean
+++ Arithmetic.lean
@@ -4,7 +4,6 @@ open Nat_
 
 theorem add_zero (m : Nat_) : m + zero = m := rfl
 
-@[simp]
 theorem add_succ (m n : Nat_) : m + succ n = succ (m + n) := rfl
 
 theorem zero_add (m : Nat_) : zero + m = m := by
@@ -14,7 +13,6 @@ theorem zero_add (m : Nat_) : zero + m = m := by
     rw [add_succ]
     rw [hyp]
 
-@[simp]
 theorem succ_add (m n : Nat_) : succ m + n = succ (m + n) := by
   induction n with
   | zero => 
@@ -35,19 +33,17 @@ theorem add_comm (m n : Nat_) : m + n = n + m := by
     rw [add_succ] 
     rw [hyp]
     
-@[simp]
 theorem add_assoc (l m n : Nat_) : (l + m) + n = l + (m + n) := by
   induction n with
   | zero => 
-    rw [add_zero]
-    rw [add_zero]
+    repeat (rw [add_zero])
   | succ n hyp => 
-    rw [add_succ]
-    rw [add_succ]
-    rw [add_succ]
+    repeat (rw [add_succ])
     rw [hyp]
 
 theorem mul_succ (m n : Nat_) : m * (succ n) = m + m * n := rfl
+
+theorem mul_zero (n : Nat_) : n * zero = zero := rfl
 
 theorem zero_mul (n : Nat_) : zero * n = zero := by
   induction n with
@@ -79,4 +75,35 @@ theorem mul_comm (m n : Nat_) : m * n = n * m := by
   | succ m hyp => 
     rw [mul_succ]
     rw [succ_mul]
+    rw [hyp]
+  
+theorem distr_r (l m n : Nat_) : (l + m) * n = l * n + m * n := by
+  induction n with
+  | zero => 
+    repeat (rw [mul_zero])
+    rfl
+  | succ n hyp =>
+    repeat (rw [mul_succ])
+    rw [hyp]
+    rw [add_assoc l (l*n) _]
+    rw [add_comm (l*n) (m + m * n)]
+    rw [add_assoc m (m*n) _]
+    rw [add_comm (m * n) (l * n)]
+    rw [←add_assoc l m _]
+
+theorem distr_l (l m n : Nat_) : l * (m + n) = l * m + l * n := by
+  rw [mul_comm]
+  rw [distr_r]
+  rw [mul_comm]
+  rw [mul_comm n]
+ 
+
+theorem mul_assoc (l m n : Nat_) : (l * m) * n = l * (m * n) := by
+  induction n with
+  | zero =>
+    repeat (rw [mul_zero])
+  | succ n hyp => 
+    rw [mul_succ m]
+    rw [mul_succ]
+    rw [distr_l]
     rw [hyp]
