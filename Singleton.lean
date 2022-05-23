8ac9111e318e42fd0616ea64f1d7175d6ab4c93f
Aiden Bailey <abailey@ABAILEY2001.local>
2022-05-23 13:50:06 -0500

    DNot

--- Singleton.lean
+++ Singleton.lean
@@ -1,14 +1,18 @@
-def Implies (a b : Prop) : Prop := a → b
-#check Implies
-
-def UDonor : Type -> Prop := by
+def UDonor : Prop -> Prop := by
   intro A
-  exact forall (B : Type), A -> B
+  exact A -> forall (B : Prop), B
 
+theorem not_eq_ud : forall (A : Prop), Not A <-> UDonor A := by
+  intro A
+  apply Iff.intro
+  intro nA a B
+  apply False.elim
+  exact nA a
+  intro udA a
+  exact udA a False
 
 #check Exists
 
 example (x : Nat) (h : x > 0) : ∃ y, y < x :=
   ⟨0, h⟩
-example (x : Nat) (h : x > 0) : ∃ y, y < x :=
-  <0, h>
+
--- Weak.lean
+++ Weak.lean
@@ -1,35 +1,35 @@
 
-def Weak : Prop -> Prop :=
+def DNot : Prop -> Prop :=
   fun A => Not (Not A)
 
-theorem weak_lem : (A : Prop) -> Weak (Or A (Not A)) := by
-  intros A not_AvnA
-  apply not_AvnA
+theorem weak_lem : (A : Prop) -> DNot (Or A (Not A)) := by
+  intro A not_lem
+  apply not_lem
   apply Or.inr
   intro a
-  apply not_AvnA
+  apply not_lem
   apply Or.inl
   exact a
 
-theorem weak_dne : (A : Prop) -> Weak (Weak A -> A) := by
-  intros A nwAtA
-  apply nwAtA
-  intro wA
+theorem weak_dne : (A : Prop) -> DNot (DNot A -> A) := by
+  intro A not_dne
+  apply not_dne
+  intro not_not_A
   apply False.elim
-  apply wA
+  apply not_not_A
   intro a
-  apply nwAtA
-  intro wA2
+  apply not_dne
+  intro ignore
   exact a
 
 open Classical
 theorem strong_lem : (A : Prop) -> (Or A (Not A)) := em
 
-theorem strong_dne : (A : Prop) -> Weak A -> A := by
-  intros A wA
+theorem strong_dne : (A : Prop) -> DNot A -> A := by
+  intro A not_not_A
   cases em A with
   | inl a => exact a
-  | inr nA =>
+  | inr not_A =>
     apply False.elim
-    exact wA nA
+    exact not_not_A not_A
 
